814eb656a7c9f44e320a4ceda2bdd6c9dc1a87c2
haozai309 <haozhanghao_87@163.com>
2014-12-25 21:29:44 +0800

    update script for functional programming

--- codecademy/anony_func.py
+++ codecademy/anony_func.py
@@ -11,3 +11,8 @@ print filter(lambda x: x == "Python", languages)
 
 squares = [x**2 for x in range(1, 11)]
 print filter(lambda x: 30 <= x <= 70, squares)
+
+garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
+
+message = filter(lambda x: x != "X", garbled)
+print message
