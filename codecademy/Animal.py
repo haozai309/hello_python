1cd611329074df032d7d8242a421f06a8ffe20c3
haozai309 <haozhanghao_87@163.com>
2014-12-27 19:53:27 +0800

    update init function for members

--- codecademy/Animal.py
+++ codecademy/Animal.py
@@ -1,4 +1,20 @@
+# Class definition
 class Animal(object):
-	""" """
-	def __init__(self):
-		pass
+	"""Makes cute animals."""
+	# For initializing our instance objects
+	def __init__(self, name, age, is_hungry):
+		self.name = name
+		self.age = age
+		self.is_hungry = is_hungry
+
+# Note that self is only used in the __init__()
+# fuction definition; we don't need to pass it
+# to our instance objects.
+
+zebra = Animal("Jeffrey", 2, True)
+giaffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
+
+print zebra.name, zebra.age, zebra.is_hungry
+print giaffe.name, giaffe.age, giaffe.is_hungry
+print panda.name, panda.age, panda.is_hungry
