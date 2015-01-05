93873f19a651b786f2413b073a9372dae7bb67a9
haozai309 <haozhanghao_87@163.com>
2015-01-05 20:45:03 +0800

    Add a sub class for car

--- codecademy/Car.py
+++ codecademy/Car.py
@@ -8,7 +8,19 @@ class Car(object):
     def display_car(self):
         print "This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg))
 
+    def drive_car(self):
+        self.condition = "used"
 
 my_car = Car("DeLorean", "silver", 88)
 print my_car.condition
-my_car.display_car()
+
+my_car.drive_car()
+print my_car.condition
+
+
+class ElectricCar(Car):
+    def __init__(self, battery_type, model, color, mpg):
+        super(ElectricCar, self).__init__(model, color, mpg)
+        self.battery_type = battery_type
+
+my_car = ElectricCar("molten salt", "Benz", "Black", 120)
